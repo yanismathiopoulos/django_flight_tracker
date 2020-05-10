b446832501fc24b0cada9b31225ed9339a2c01d3
ymathiopoulos <ymathiopoulos@gmail.com>
2020-05-10 14:01:30 +0200

    some settings

--- django_flight_tracker/settings.py
+++ django_flight_tracker/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('./etc/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+# with open('./etc/secret_key.txt') as f:
+#     SECRET_KEY = f.read().strip()
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
